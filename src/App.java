caf76c3f4e1b78a9337fb969962a07eda6b50429
Corentin <corentin.laurent@gmail.com>
2023-02-02 21:56:10 +0100

    First group commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, corentin!");
+        System.out.println("Hello, Corentin, Dani and Sosthene!");
     }
 }
